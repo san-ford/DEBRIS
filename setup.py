34fdfdcbc7964206713d33146f20149ebf1b9185
Kevin Sanford <ksanf003@ucr.edu>
2025-07-22 21:12:35 -0700

    Create setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+
+if sys.platform == "darwin" or sys.platform == "linux":
+    cmd = "python3"
+elif sys.platform == "win32":
+    cmd = "py"
+else:
+    raise ValueError("Operating system not compatible")
+
+subprocess.run([cmd, "debris/manage.py", "makemigrations"])
+subprocess.run([cmd, "debris/manage.py", "migrate"])
+subprocess.run([cmd, "populate.py"])
+subprocess.run([cmd, "debris/manage.py", "runserver"])
